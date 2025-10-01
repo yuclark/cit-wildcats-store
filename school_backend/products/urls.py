9f564608ac482b546851f1e8466c594ef3c7aa7b
Vince Clark B. Lanticse <lanticsev@gmail.com>
2025-10-01 19:05:45 +0800

    found culprit

--- school_backend/products/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.ProductListView.as_view(), name='product-list'),
-    path('<int:pk>/', views.ProductDetailView.as_view(), name='product-detail'),
-    path('categories/', views.CategoryListView.as_view(), name='category-list'),
-]
